Correct dice computation

UWMGI_Unet_3D.py
@@ -23,200 +23,61 @@ from tensorflow.keras.losses import *
 from tensorflow.keras.metrics import *
 import tensorflow.keras.backend as K
 from sklearn.model_selection import GroupKFold
-from segmentation_models.losses import *
 
-
-from image_helper import open_image, center_padding_3d, restore_original_3d, rle_decode
-from metrics import dice_large_bowel, dice_stomach, dice_small_bowel, Dice_Coef
+from metrics import Dice_Coef, bce_dice_loss
 from unet3d import Unet3D
-from utils import seed_everything
+from dataloader import DataLoader
+from utils import seed_everything, poly_scheduler, cosine_scheduler, preprocess_dataframe
 from augment3d import Augment3D
 
 '''
-    Reproducability
-'''
-seed_everything()
-
-'''
-    Arguments parsers
+    PARSE ARGUMENTS
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument("--description", type=str, help="model description", default="baseline")
-parser.add_argument("--datafolder", type=str, help="Data folder", default="./preprocessed_144x384x384")
+parser.add_argument("--backbone", type=str, help="Unet backbone", default="unet3d")
+parser.add_argument("--description", type=str, help="Model description", default="3d")
 parser.add_argument("--batch", type=int, help="Batch size", default=16)
+parser.add_argument("--datafolder", type=str, help="Data folder", default='preprocessed_144x384x384')
+parser.add_argument("--seed", type=int, help="Seed for random generator", default=2022)
+parser.add_argument("--csv", type=str, help="Dataframe path", default='preprocessed_train.csv')
+parser.add_argument("--trainsize", type=str, help="Training image size", default="80x224x224x1")
+parser.add_argument("--validsize", type=str, help="Validation image size", default="144x384x384x1")
+parser.add_argument("--unet", type=str, help="Unet conv settings", default="16x32x64x128x256")
+parser.add_argument("--resnet", type=int, help="Unet residual conv repeats", default=2)
+parser.add_argument("--fold", type=int, help="Number of folds", default=5)
+parser.add_argument("--epoch", type=int, help="Number of epochs", default=150)
 args = parser.parse_args()
 
 '''
     CONFIGURATION
 '''
-TRAIN_DIR = './train/' # case/cases_day/scans
-preprocessed_folder = args.datafolder
-df = pd.read_csv("./preprocessed_train.csv")
+RANDOM_SEED = args.seed
+DATAFOLDER = args.datafolder
+DF = pd.read_csv(args.csv)
 
 MODEL_CHECKPOINTS_FOLDER = './model_checkpoint/'
-MODEL_NAME = "UNET_3D"
+MODEL_NAME = args.backbone
 MODEL_DESC = args.description
 
-NUM_CLASSES = 3
-TRAINING_PATCH_SIZE = (64, 224, 224) # First channel = IMAGE + PREDICTED MASK FROM 2D MODEL. SO PATCH 1ST DIMENSION IS 32 * 32 * NUM_CLASSES
-VALIDATION_PATCH_SIZE = (144, 384, 384)
-AUGMENT = Augment3D(TRAINING_PATCH_SIZE[0])
+TRAINING_SIZE = tuple([int(x) for x in args.trainsize.split("x")])
+VALID_SIZE = tuple([int(x) for x in args.validsize.split("x")])
 BATCH_SIZE = args.batch
-KFOLD = 5
+KFOLD = args.fold
+NUM_CLASSES = 3
 
+augment = Augment3D(TRAINING_SIZE[0], TRAINING_SIZE[1], TRAINING_SIZE[2])
 
-UNET_FILTERS = [16, 32, 64, 128, 256]
-REPEATS = [2, 2, 2, 2, 2]
-initial_lr = 1e-2
-min_lr = 1e-5
-no_of_epochs = 100
+UNET_FILTERS = [int(x) for x in args.unet.split("x")]
+REPEATS = [args.resnet] * len(UNET_FILTERS)
+initial_lr = 1e-3
+min_lr = 1e-6
+no_of_epochs = args.epoch
 epochs_per_cycle = no_of_epochs
 
 class_map = ["large_bowel", "small_bowel", "stomach"]
 
-if not os.path.exists(f"{preprocessed_folder}"):
-    os.makedirs(f"{preprocessed_folder}")
-
-if not os.path.exists(f"{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}"):
-    os.makedirs(f"{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}")
-    
-if not os.path.exists(f"plot/{MODEL_NAME}"):
-    os.makedirs(f"plot/{MODEL_NAME}")
-
-
 '''
-    Preprocessing  dataframe
-'''
-df_train = pd.DataFrame({'id':df['id'][::3]})
-
-df_train['large_bowel'] = df['segmentation'][::3].values
-df_train['small_bowel'] = df['segmentation'][1::3].values
-df_train['stomach'] = df['segmentation'][2::3].values
-
-df_train['path'] = df['path'][::3].values
-df_train['case'] = df['case'][::3].values
-df_train['day'] = df['day'][::3].values
-df_train['slice'] = df['slice'][::3].values
-
-df_train.reset_index(inplace=True,drop=True)
-df_train.fillna('',inplace=True); 
-df_train['count'] = np.sum(df_train.iloc[:,1:4]!='',axis=1).values
-df_train.head()
-
-'''
-    Data loader
-'''
-from tensorflow.keras.utils import *
-
-class DataLoader(Sequence):
-    def __init__(self, train_ids, batch_size = BATCH_SIZE, shuffle = False, augment = None ):
-        self.train_ids = train_ids
-        self.batch_size = batch_size 
-        self.indices = np.arange(len(train_ids))
-        self.augment = augment
-        self.shuffle = shuffle
-        
-    def load_data(self, train_id):
-        X = np.load(f"{preprocessed_folder}/{train_id}.npy")
-        y = np.load(f"{preprocessed_folder}/{train_id}_mask.npy")
-        if len(X.shape) == 3:
-            X = np.expand_dims(X, axis=-1)
-        if self.shuffle:
-            # Get random patches
-            d = h = w = 0
-
-            if X.shape[0] > TRAINING_PATCH_SIZE[0]:
-                d = np.random.randint(0, X.shape[0] - TRAINING_PATCH_SIZE[0])
-            if X.shape[1] > TRAINING_PATCH_SIZE[1]:
-                h = np.random.randint(0, X.shape[1] - TRAINING_PATCH_SIZE[1])
-            if X.shape[2] > TRAINING_PATCH_SIZE[2]:
-                w = np.random.randint(0, X.shape[2] - TRAINING_PATCH_SIZE[2])
-
-            X = X[d:d + TRAINING_PATCH_SIZE[0], h:h + TRAINING_PATCH_SIZE[1], w:w + TRAINING_PATCH_SIZE[2]]
-            y = y[d:d + TRAINING_PATCH_SIZE[0], h:h + TRAINING_PATCH_SIZE[1], w:w + TRAINING_PATCH_SIZE[2]]
-        return X, y
-
-    def __len__(self):
-        if len(self.indices) % self.batch_size == 0 or self.shuffle:
-            return len(self.indices) // self.batch_size
-        return len(self.indices) // self.batch_size + 1
-
-    def on_epoch_end(self):
-        if self.shuffle:
-            np.random.shuffle(self.indices)
-
-    def __getitem__(self, index):
-        indices = self.indices[index * self.batch_size : (index + 1) * self.batch_size]
-
-        X = np.empty((len(indices), *TRAINING_PATCH_SIZE, 1), dtype='float32')
-        y = np.empty((len(indices), *TRAINING_PATCH_SIZE, 3), dtype='float32')
-        if not self.shuffle:
-            X = np.empty((len(indices), *VALIDATION_PATCH_SIZE, 1), dtype='float32')
-            y = np.empty((len(indices), *VALIDATION_PATCH_SIZE, 3), dtype='float32')
-            
-        for i in range(len(indices)):
-            train_id = self.train_ids[indices[i]]
-            image, mask = self.load_data(train_id)
-            if self.augment is not None:
-                X[i], y[i] = self.augment(image, mask)
-            else:
-                X[i], y[i] = image, mask
-        return X, y
-
-def poly_scheduler(epoch, lr, exponent = 0.9):
-    return max(initial_lr * (1 - epoch / no_of_epochs)**exponent, min_lr)
-
-def cosine_scheduler(epoch, lr):
-    return min_lr + (initial_lr - min_lr) * (1 + np.cos(np.pi * (epoch % epochs_per_cycle) / epochs_per_cycle)) / 2
-
-skf = GroupKFold(n_splits=KFOLD)
-for fold, (_, val_idx) in enumerate(skf.split(X=df_train, groups =df_train['case']), 1):
-    df_train.loc[val_idx, 'fold'] = fold
-df_train.head()
-
-all_files = os.listdir(f"{preprocessed_folder}")
-all_files_wo_mask = [x for x in all_files if 'mask' not in x]
-cases_days = [os.path.splitext(x)[0] for x in all_files_wo_mask]
-
-hists = []
-for fold in range(1, KFOLD + 1):
-    K.clear_session()
-    gc.collect()
-    
-    train_case = [f"case{x}" for x in df_train["case"][df_train["fold"] != fold].values]
-    test_case  = [f"case{x}" for x in df_train["case"][df_train["fold"] == fold].values]
-    
-    train_id = [x for x in cases_days if x.split("_")[0] in train_case]
-    test_id = [x for x in cases_days if x.split("_")[0] in test_case]
-                 
-    train_datagen = DataLoader(train_id, batch_size=BATCH_SIZE, shuffle=True, augment=None)
-    test_datagen = DataLoader(test_id, batch_size=1, shuffle=False, augment=None)
-    
-    model = Unet3D(conv_settings = UNET_FILTERS, repeat = REPEATS, input_shape = (None, None, None, 1), num_classes = 3)()
-    model.summary(line_length=150)
-
-    optimizer = SGD(momentum=0.9, nesterov=True)
-    optimizer = tfa.optimizers.SWA(optimizer)
-
-    tf.config.optimizer.set_jit(True)
-    model.compile(optimizer=optimizer, loss=bce_dice_loss, metrics=[dice_large_bowel, dice_small_bowel, dice_stomach, Dice_Coef])
-    
-    callbacks = [
-        ModelCheckpoint(f'{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}/{MODEL_DESC}_fold{fold}.h5', verbose=1, save_best_only=True, monitor="val_Dice_Coef", mode='max'),
-        LearningRateScheduler(schedule=cosine_scheduler, verbose=1),
-        CSVLogger(f'{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}/{MODEL_DESC}_fold{fold}.csv', separator=",", append=False)
-    ]
-
-    hist = model.fit_generator(train_datagen, 
-                               epochs=no_of_epochs, 
-                               callbacks = callbacks,
-                               validation_data=test_datagen,
-                               verbose=2)
-    hists.append(hist)
-    # break
-
-'''
-    Plot learning curves
+    PLOTTING FUNCTIONS
 '''
 def plot_training_result(history, fold):
     plt.figure(figsize = (8,6))
@@ -227,7 +88,6 @@ def plot_training_result(history, fold):
     plt.title(f'Fold {fold}: Loss')
     plt.legend()
     plt.savefig(f"plot/{MODEL_NAME}/{MODEL_DESC}_fold{fold}_loss.png")
-    plt.show()
     
     plt.figure(figsize = (8,6))
     plt.plot(history.history['Dice_Coef'], '-', label = 'train_Dice_coef', color = 'g')
@@ -237,12 +97,77 @@ def plot_training_result(history, fold):
     plt.title(f'Fold {fold}: Dice Coef')
     plt.legend()
     plt.savefig(f"plot/{MODEL_NAME}/{MODEL_DESC}_fold{fold}_DC.png")
-    plt.show()
 
-val_Dice_Coef = []
-for i in range(1, KFOLD + 1):
-    plot_training_result(hists[i-1], i)
-    val_Dice_Coef.append(np.max(hists[i-1].history['val_Dice_Coef']))
-    # break
+'''
+    MAIN PROGRAM
+'''
+if __name__ == "__main__":
+    # REPRODUCIBILITY
+    seed_everything(RANDOM_SEED)
 
-print(f"{np.mean(val_Dice_Coef)} +- {np.std(val_Dice_Coef)}")
+    print(f"Model name: {MODEL_NAME}. Description: {MODEL_DESC}")
+    class_map = ["large_bowel", "small_bowel", "stomach"]
+
+    if not os.path.exists(f"{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}"):
+        os.makedirs(f"{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}")
+        
+    if not os.path.exists(f"plot/{MODEL_NAME}"):
+        os.makedirs(f"plot/{MODEL_NAME}")
+
+    # PREPROCESSING DATAFRAME
+    DF = preprocess_dataframe(DF)
+
+    # SPLIT DATA INTO KFOLD
+    skf = GroupKFold(n_splits=KFOLD)
+    for fold, (_, val_idx) in enumerate(skf.split(X=DF, groups =DF['case']), 1):
+        DF.loc[val_idx, 'fold'] = fold
+
+    all_files = os.listdir(f"{DATAFOLDER}")
+    all_files_wo_mask = [x for x in all_files if 'mask' not in x]
+    cases_days = [os.path.splitext(x)[0] for x in all_files_wo_mask]
+
+    hists = []
+
+    # TRAINING FOR KFOLD
+    for fold in range(1, KFOLD + 1):
+        # Clear sessions and collect garbages
+        K.clear_session()
+        gc.collect()
+
+        # Enable XLA for performance
+        tf.config.optimizer.set_jit(True)
+
+        train_case = [f"case{x}" for x in DF["case"][DF["fold"] != fold].values]
+        test_case  = [f"case{x}" for x in DF["case"][DF["fold"] == fold].values]
+        
+        train_id = [x for x in cases_days if x.split("_")[0] in train_case]
+        test_id = [x for x in cases_days if x.split("_")[0] in test_case]
+        train_datagen = DataLoader(train_id, TRAINING_SIZE, (*TRAINING_SIZE[:-1], NUM_CLASSES), DATAFOLDER, batch_size=BATCH_SIZE, shuffle=True, augment=augment)
+        test_datagen = DataLoader(test_id, VALID_SIZE, (*VALID_SIZE[:-1], NUM_CLASSES), DATAFOLDER, batch_size=1, shuffle=False, augment=None)
+        
+        model = Unet3D(conv_settings=UNET_FILTERS)()
+        
+        model.compile(optimizer=Adam(), loss=bce_dice_loss(axis=(0,1,2,3)), metrics=[Dice_Coef(axis=(0,1,2,3))])
+        
+        callbacks = [
+            ModelCheckpoint(f'{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}/{MODEL_DESC}_fold{fold}.h5', verbose=1, save_best_only=True, monitor="val_Dice_Coef", mode='max'),
+            LearningRateScheduler(schedule=cosine_scheduler(initial_lr, min_lr, no_of_epochs), verbose=1),
+            CSVLogger(f'{MODEL_CHECKPOINTS_FOLDER}/{MODEL_NAME}/{MODEL_DESC}_fold{fold}.csv', separator=",", append=False)
+        ]
+        hist = model.fit_generator(train_datagen, 
+                                epochs=no_of_epochs, 
+                                callbacks = callbacks,
+                                validation_data=test_datagen,
+                                verbose=2)
+        hists.append(hist)
+
+    # PLOT TRAINING RESULTS
+    val_Dice_Coef = []
+
+    for i in range(1, KFOLD + 1):
+        plot_training_result(hists[i-1], i)
+        val_Dice_Coef.append(np.max(hists[i-1].history['val_Dice_Coef']))
+
+    print(val_Dice_Coef)
+    print(f"{np.mean(val_Dice_Coef)} +- {np.std(val_Dice_Coef)}")
+    print("Done!")

augment3d.py
@@ -3,16 +3,36 @@ import albumentations as A
 import cv2
 
 class Augment3D:
-    def __init__(self, image_depth):
+    def __init__(self, image_depth, image_width, image_height):
         keys = {f'image{i}' : 'image' for i in range(1, image_depth)}
         keys.update({f'mask{i}' : 'mask' for i in range(1, image_depth)})
         self.transform = A.Compose([
-                                    A.Flip(p=0.5),
+                                    A.HorizontalFlip(p=0.5),
                                     A.ShiftScaleRotate(p=0.5, border_mode=cv2.BORDER_CONSTANT, value=0., mask_value=0.),
                                     A.GridDistortion(p=0.2, border_mode=cv2.BORDER_CONSTANT, value=0., mask_value=0.)
                                    ], additional_targets=keys)
+        self.d = image_depth
+        self.w = image_width
+        self.h = image_height
     
     def __call__(self, image, mask):
+        # Generate random patches 
+        d, h, w = image.shape[:3]
+        start_d = start_h = start_w = 0
+        if d > self.d:
+            start_d = np.random.randint(0, d - self.d)
+        if h > self.h:
+            start_h = np.random.randint(0, h - self.h)
+        if w > self.w:
+            start_w = np.random.randint(0, w - self.w)
+
+        image = image[start_d:start_d + self.d, start_h:start_h + self.h, start_w:start_w + self.w]
+        mask = mask[start_d:start_d + self.d, start_h:start_h + self.h, start_w:start_w + self.w]
+
+        # Add channel axis if necessary
+        if len(image.shape) < 4:
+            image = np.expand_dims(image, axis=-1)
+
         data = {f'image{i}' : image[i] for i in range(1, image.shape[0])}
         data.update({f'mask{i}' : mask[i] for i in range(1, image.shape[0])})
         data["image"] = image[0]

metrics.py
@@ -1,31 +1,37 @@
-import tensorflow as tf
 import math
+
+import tensorflow as tf
 import tensorflow.keras.backend as K
+from tensorflow.keras.losses import *
+from segmentation_models.metrics import *
 
-# Metrics
-def dice_large_bowel(y_true, y_pred, smooth = 1e-6):
-    y_true_f = K.flatten(y_true[..., 0])
-    y_pred_f = tf.math.round(K.flatten(y_pred[..., 0]))
-    
-    intersection = K.sum(y_true_f * y_pred_f)
-    dice = (2*intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return dice
+'''
+    METRICS AND LOSS FUNCTIONS
+    Dice Coefficient = 2 * tp / (2*tp + fn + fp)
+'''
+def Dice_Coef(axis=(0,1,2), smooth=1e-6):
+    def Dice_Coef(y_true, y_pred):
+        y_pred = tf.math.round(y_pred)
+        tp = tf.math.reduce_sum(y_true * y_pred, axis=axis) # calculate True Positive
+        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis) # calculate False Negative
+        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis) # calculate False Positive
+        numerator = 2 * tp + smooth
+        denominator = 2 * tp + fn + fp + smooth
+        return tf.math.reduce_mean(numerator / denominator)
+    return Dice_Coef
 
-def dice_small_bowel(y_true, y_pred, smooth = 1e-6):
-    y_true_f = K.flatten(y_true[..., 1])
-    y_pred_f = tf.math.round(K.flatten(y_pred[..., 1]))
-    
-    intersection = K.sum(y_true_f * y_pred_f)
-    dice = (2*intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return dice
+def dice_loss(axis = (0,1,2), smooth = 1e-6):
+    def dice_loss(y_true, y_pred):
+        tp = tf.math.reduce_sum(y_true * y_pred, axis=axis) # calculate True Positive
+        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis) # calculate False Negative
+        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis) # calculate False Positive
+        numerator = 2 * tp + smooth
+        denominator = 2 * tp + fn + fp + smooth
+        return tf.math.reduce_mean(1 - numerator / denominator) # Average over classes
+    return dice_loss
 
-def dice_stomach(y_true, y_pred, smooth = 1e-6):
-    y_true_f = K.flatten(y_true[..., 2])
-    y_pred_f = tf.math.round(K.flatten(y_pred[..., 2]))
-    
-    intersection = K.sum(y_true_f * y_pred_f)
-    dice = (2*intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return dice
-
-def Dice_Coef(y_true, y_pred):
-    return (dice_large_bowel(y_true, y_pred) + dice_small_bowel(y_true, y_pred) + dice_stomach(y_true, y_pred)) / 3
+def bce_dice_loss(axis = (0, 1, 2),  smooth = 1e-6):
+    dice_loss_func = dice_loss(axis, smooth)
+    def loss(y_true, y_pred):
+        return binary_crossentropy(y_true, y_pred) + dice_loss_func(y_true, y_pred)
+    return loss