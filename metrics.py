a96b8b99ed86c994d143371695d0b234eae16bae
quandapro <quandapro@gmail.com>
2022-06-22 12:35:34 +0900

    Ignore if X, y are both empty

--- metrics.py
+++ metrics.py
@@ -10,31 +10,33 @@ from monai.metrics.utils import get_mask_edges, get_surface_distance
 
 '''
     METRICS AND LOSS FUNCTIONS
-    Dice Coefficient = 2 * tp / (2*tp + fn + fp)
 '''
-def Dice_Coef(axis=(0,1,2), smooth=1e-6):
+def Dice_Coef(spartial_axis = (1,2), mean_axis=(1, 0), smooth=1e-6):
     def Dice_Coef(y_true, y_pred):
         y_pred = tf.math.round(y_pred)
-        tp = tf.math.reduce_sum(y_true * y_pred, axis=axis) # calculate True Positive
-        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis) # calculate False Negative
-        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis) # calculate False Positive
-        numerator = 2 * tp + smooth
-        denominator = 2 * tp + fn + fp + smooth
-        return tf.math.reduce_mean(numerator / denominator)
+        tp = tf.math.reduce_sum(y_true * y_pred, axis=spartial_axis) # calculate True Positive
+        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=spartial_axis) # calculate False Negative
+        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=spartial_axis) # calculate False Positive 
+        numerator = 2 * tp
+        denominator = 2 * tp + fn + fp
+
+        nonzero = tf.math.count_nonzero(denominator)
+        nonzero = tf.cast(nonzero, dtype=tf.float32)
+        return tf.math.reduce_sum(numerator / (denominator + smooth) ) / nonzero # Ignore cases where both masks are empty
     return Dice_Coef
 
-def dice_loss(axis = (0,1,2), smooth = 1e-6):
+def dice_loss(spartial_axis=(1,2,3), mean_axis=(1, 0), smooth = 1e-6):
     def dice_loss(y_true, y_pred):
-        tp = tf.math.reduce_sum(y_true * y_pred, axis=axis) # calculate True Positive
-        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=axis) # calculate False Negative
-        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=axis) # calculate False Positive
+        tp = tf.math.reduce_sum(y_true * y_pred, axis=spartial_axis) # calculate True Positive
+        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=spartial_axis) # calculate False Negative
+        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=spartial_axis) # calculate False Positive
         numerator = 2 * tp + smooth
         denominator = 2 * tp + fn + fp + smooth
-        return tf.math.reduce_mean(1 - numerator / denominator) # Average over classes
+        return tf.math.reduce_mean(1 - numerator / denominator, mean_axis) # Average over classes
     return dice_loss
 
-def bce_dice_loss(axis = (0, 1, 2),  smooth = 1e-6):
-    dice_loss_func = dice_loss(axis, smooth)
+def bce_dice_loss(spartial_axis=(1,2,3), mean_axis=(1, 0),  smooth = 1e-6):
+    dice_loss_func = dice_loss(spartial_axis, mean_axis, smooth)
     def loss(y_true, y_pred):
         return binary_crossentropy(y_true, y_pred) + dice_loss_func(y_true, y_pred)
     return loss
