a4f01982334dd73ae08595d358cfb7e7d1a330d8
quandapro <quandapro@gmail.com>
2022-07-01 13:59:39 +0900

    Sliding window task

--- metrics.py
+++ metrics.py
@@ -41,56 +41,119 @@ def bce_dice_loss(spartial_axis=(1,2,3), smooth = 1e-6):
         return binary_crossentropy(y_true, y_pred) + dice_loss_func(y_true, y_pred)
     return loss
 
-def compute_hausdorff_monai(y_true, y_pred, max_dist):
-    if np.all(y_pred == y_true):
+def tversky_loss(alpha=0.5, spartial_axis=(1,2,3), smooth = 1e-6):
+    '''
+        Tversky loss
+        High alpha -> Emphasize on false positive / Specificity
+        Low alpha -> Emphasize on false negative / Sensitivity
+    '''
+    def tversky_loss(y_true, y_pred):
+        tp = tf.math.reduce_sum(y_true * y_pred, axis=spartial_axis) # calculate True Positive
+        fn = tf.math.reduce_sum(y_true * (1 - y_pred), axis=spartial_axis) # calculate False Negative
+        fp = tf.math.reduce_sum((1 - y_true) * y_pred, axis=spartial_axis) # calculate False Positive
+        numerator = tp + smooth
+        denominator = tp + alpha * fp + (1 - alpha) * fn + smooth
+        return tf.math.reduce_mean(1 - numerator / denominator) # Average over classes
+    return tversky_loss
+
+def bce_tversky_loss(alpha=0.5, spartial_axis=(1,2,3), smooth = 1e-6):
+    tversky_loss_func = tversky_loss(alpha, spartial_axis, smooth)
+    def loss(y_true, y_pred):
+        return binary_crossentropy(y_true, y_pred) + tversky_loss_func(y_true, y_pred)
+    return loss
+
+def compute_directed_hausdorff(pred, gt, max_dist):
+    if np.all(pred == gt):
+        return 0.0
+    if np.sum(pred) == 0:
         return 1.0
-    (edges_pred, edges_gt) = get_mask_edges(y_pred, y_true)
+    if np.sum(gt) == 0:
+        return 1.0
+    (edges_pred, edges_gt) = get_mask_edges(pred, gt)
     surface_distance = get_surface_distance(edges_pred, edges_gt, distance_metric="euclidean")
     if surface_distance.shape == (0,):
-        return 1.0
-    dist = surface_distance.max()
-    if dist > max_dist:
         return 0.0
-    return 1 - dist / max_dist
+    dist = surface_distance.max()
+
+    if dist > max_dist:
+        return 1.0
+    return dist / max_dist
 
 def hausdorff(y_true, y_pred, max_dist):
     result = []
     for i in range(y_true.shape[-1]):
-        result.append(compute_hausdorff_monai(y_true[..., i], y_pred[..., i], max_dist))
+        result.append(1.0 - compute_directed_hausdorff(y_pred[..., i], y_true[..., i], max_dist))
     return np.mean(result)
 
-def dice_coef_numpy(y_true, y_pred, axis=(0,1,2), smooth = 1e-6):
+def dice_coef_numpy(y_true, y_pred, axis=(1,2), smooth = 1e-6, ignore_empty=True):
     y_pred = np.round(y_pred)
     tp = np.sum(y_true * y_pred, axis=axis) # calculate True Positive
     fn = np.sum(y_true * (1 - y_pred), axis=axis) # calculate False Negative
     fp = np.sum((1 - y_true) * y_pred, axis=axis) # calculate False Positive
-    numerator = 2 * tp + smooth
-    denominator = 2 * tp + fn + fp + smooth
-    return np.mean(numerator / denominator)
+    numerator = 2 * tp
+    denominator = 2 * tp + fn + fp
+    if ignore_empty:
+        non_empty = float(np.count_nonzero(denominator))
+        return np.sum(numerator / (denominator + smooth)) / (non_empty + smooth)
+    return np.mean((numerator + smooth) / (denominator + smooth))
 
 class CompetitionMetric(Callback):
-    def __init__(self, validation_data, model_checkpoint, shape = (144, 384, 384)):
+    def __init__(self, validation_data, model_checkpoint, period = 10, deep_supervision = True, shape = (144, 266, 266)):
         super(Callback, self).__init__()
         
         self.validation_data = validation_data
         self.model_checkpoint = model_checkpoint
         self.best_validation_score = -np.inf
         self.max_dists = np.sqrt(np.sum([x ** 2 for x in shape]))
+        self.deep_supervision = deep_supervision
+        self.period = period
+        self.history = {
+            "val_dice_coef" : [],
+            "val_hausdorff" : [],
+            "val_score"     : []
+        } 
         
+    def sliding_window_inference(self, image, model, window_size=(64, 224, 224), stride=(32, 112, 112)):
+        '''
+            Sliding window inference
+            image : numpy.ndarray
+                Input 3D volume with shape = (Batch, Depth, Height, Width, Channel)
+            model : tf.keras.Model
+                Inference model
+            window_size : Tuple
+                Window size
+            stride : Tuple
+                Stride
+        '''
+        return None
+
     def on_epoch_end(self, epoch, logs={}):
-        dice_coef = []
-        hausdorff_metric = []
-        comp_metric = []
-        for X, y_true in self.validation_data:
-            y_pred = self.model.predict(X)
-            dice_coef.append(dice_coef_numpy(y_true[0], y_pred[0]))
-            hausdorff_metric.append(hausdorff(y_true[0], y_pred[0], self.max_dists))
-            comp_metric.append(0.4 * dice_coef[-1] + 0.6 * hausdorff_metric[-1])
-        mean_dice_coef = np.mean(dice_coef)
-        mean_hausdorff_metric = np.mean(hausdorff_metric)
-        result = np.mean(comp_metric)
-        print(f"val_dice_coef: {mean_dice_coef}; val_hausdorff_metric: {mean_hausdorff_metric}; val_score: {result}")
-        if result > self.best_validation_score:
-            print(f"Validation score improved from {self.best_validation_score} to {result}. Saving model...")
-            self.model.save_weights(self.model_checkpoint)
-            self.best_validation_score = result
+        if (epoch + 1) % self.period == 0:
+            dice_coef = []
+            hausdorff_metric = []
+            comp_metric = []
+            for X, y_true in self.validation_data:
+                y_pred = self.model.predict(X)
+                if self.deep_supervision:
+                    y_pred = y_pred[-1]
+                # Thresholding
+                y_pred = (y_pred > 0.5).astype('float32')
+                dice_coef.append(dice_coef_numpy(y_true[0], y_pred[0]))
+                hausdorff_metric.append(hausdorff(y_true[0], y_pred[0], self.max_dists))
+                comp_metric.append(0.4 * dice_coef[-1] + 0.6 * hausdorff_metric[-1])
+
+            mean_dice_coef = np.mean(dice_coef)
+            mean_hausdorff_metric = np.mean(hausdorff_metric)
+            result = np.mean(comp_metric)
+            
+            self.history["val_dice_coef"].append(mean_dice_coef)
+            self.history["val_hausdorff"].append(mean_hausdorff_metric)
+            self.history["val_score"].append(result)
+
+            print(f"val_dice_coef: {mean_dice_coef}; val_hausdorff_metric: {mean_hausdorff_metric}; val_score: {result}")
+            if result > self.best_validation_score:
+                print(f"Validation score improved from {self.best_validation_score} to {result}. Saving model...")
+                self.model.save_weights(self.model_checkpoint)
+                self.best_validation_score = result
+            else:
+                print(f"Validation score does not improve from: {self.best_validation_score}")
